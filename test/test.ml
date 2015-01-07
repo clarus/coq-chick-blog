dc59f09367a035ac0fdb19a1d37c5ea48b7ac57c
Guillaume Claret <dev@clarus.me>
2015-01-07 18:18:44 +0100

    Matching on the request path

--- test/test.ml
+++ test/test.ml
@@ -2,7 +2,10 @@ open Cohttp_lwt_unix
 
 let callback (connection : Server.conn) (request : Cohttp.Request.t)
   (body : Cohttp_lwt_body.t) : (Cohttp.Response.t * Cohttp_lwt_body.t) Lwt.t =
-  Server.respond_string `OK "Hello" ()
+  let uri = Cohttp.Request.uri request in
+  match Uri.path uri with
+  | "/" -> Server.respond_string `OK "index" ()
+  | path -> Server.respond_string `Not_found (path ^ " not found") ()
 
 let main () =
   let config = Server.make callback () in
