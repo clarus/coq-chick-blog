31d8c727ee9bf70c15ceb3cec8ae62c041792f1f
Guillaume Claret <dev@clarus.me>
2015-01-12 20:13:37 +0100

    Unused code removed

--- extraction/http.ml
+++ extraction/http.ml
@@ -1,12 +1,3 @@
-let requests_push_request =
-  Lwt_react.E.create ()
-
-let requests : string React.event =
-  fst requests_push_request
-
-let push (path : string) : unit =
-  (snd requests_push_request) path
-
 let start_server
   (handler : string list -> (string * string list) list ->
     (string * string) Lwt.t)
