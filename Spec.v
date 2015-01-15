6947eec3f84482e714c06c2e1a95080527d6efc6
Guillaume Claret <dev@clarus.me>
2015-01-15 16:54:08 +0100

    First specification proven (for the index page)

--- Spec.v
+++ Spec.v
@@ -5,6 +5,7 @@ Require Import ListString.All.
 Require Import Computation.
 Require Http.
 Require Main.
+Require Import Model.
 Require Request.
 
 Import ListNotations.
@@ -27,41 +28,13 @@ Module FiniteRun.
   Definition t := list RunRequest.t.
 End FiniteRun.
 
-(*Definition index : RunRequest.t.
-  apply (RunRequest.New (Request.Raw.New [] [] [])).
-  simpl.
-  apply (Run.Call (Command.Log _) tt).
-Admitted.*)
-
-(*
-(** Get one page and compare its result with `answer`. *)
-Ltac static_page path args answer :=
-  apply cons; [
-  apply (RunRequest.New (Http.Request.Get path args));
-  apply (Run.Let (Command.Log _) tt);
-  exact (Run.Ret answer) |
-  exact nil].
-
-Ltac static_page_no_args path answer :=
-  static_page path (nil (A := LString.t * list LString.t)) answer.
-
-Definition unknown_page : FiniteRun.t.
-  static_page_no_args [LString.s "hello"] Http.Answer.Error.
+Definition index (cookies : Request.Cookies.t)
+  (post_headers : list Post.Header.t) : RunRequest.t.
+  apply (RunRequest.New (Request.New Request.Path.Index cookies)).
+  unfold Main.server; simpl.
+  unfold Main.Controller.index; simpl.
+  apply (Run.Call (Command.ListPosts _ ) (Some post_headers)).
+  exact (Run.Ret (Answer.Public
+    (Request.Cookies.is_logged @@ cookies)
+    (Answer.Public.Index post_headers))).
 Defined.
-
-Definition index (file_names : list LString.t) : FiniteRun.t.
-  apply cons.
-  - apply (RunRequest.New (Http.Request.Get [] [])).
-    simpl.
-    apply (Run.Let (Command.Log _) tt).
-    unfold Main.Controller.index; simpl.
-    apply (Run.Let (Command.ListFiles _) (Some file_names)).
-    refine (let posts := file_names |> List.map (fun file_name =>
-      Post.Header.New file_name (LString.s "date") file_name) in _).
-    exact (Run.Ret (Http.Answer.Index posts)).
-  - exact nil.
-Defined.
-
-Definition args (args : Arguments.t) : FiniteRun.t.
-  static_page [LString.s "args"] args (Http.Answer.Args args).
-Defined.*)
