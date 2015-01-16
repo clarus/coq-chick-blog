b873745c7efcef157093862759c9568a3d2a24c2
Guillaume Claret <dev@clarus.me>
2015-01-16 16:50:34 +0100

    Comments for utils.ml

--- extraction/utils.ml
+++ extraction/utils.ml
@@ -1,7 +1,10 @@
+(** Some OCaml primitives for the extraction. *)
 open Big_int
 
+(** Interface to the OCaml strings. *)
 module String = struct
-  let to_lstring s =
+  (** Export an OCaml string. *)
+  let to_lstring (s : string) : char list =
     let rec aux l i =
       if i = -1 then
         l
@@ -9,13 +12,15 @@ module String = struct
         aux (s.[i] :: l) (i - 1) in
     aux [] (String.length s - 1)
 
-  let of_lstring s =
+  (** Import a Coq string. *)
+  let of_lstring (s : char list) : string =
     let length = List.length s in
     let buffer = String.create length in
     List.iteri (fun i c -> String.set buffer i c) s;
     buffer
 end
 
+(** Read the content of a file. *)
 let read_file (file_name : string) : string option Lwt.t =
   Lwt.catch (fun _ ->
     Lwt.bind (Lwt_io.open_file Lwt_io.Input file_name) (fun channel ->
@@ -24,6 +29,7 @@ let read_file (file_name : string) : string option Lwt.t =
     Lwt.return @@ Some content))))
     (fun _ -> Lwt.return None)
 
+(** Update (or create) a file with some content. *)
 let update_file (file_name : string) (content : string) : bool Lwt.t =
   Lwt.catch (fun _ ->
     Lwt.bind (Lwt_io.open_file Lwt_io.Output file_name) (fun channel ->
@@ -32,12 +38,14 @@ let update_file (file_name : string) (content : string) : bool Lwt.t =
     Lwt.return true))))
     (fun _ -> Lwt.return false)
 
+(** Delete a file. *)
 let delete_file (file_name : string) : bool Lwt.t =
   Lwt.catch (fun _ ->
     Lwt.bind (Lwt_unix.unlink file_name) (fun _ ->
     Lwt.return true))
     (fun _ -> Lwt.return false)
 
+(** List the files of a directory. *)
 let list_files (directory : string) : string list option Lwt.t =
   Lwt.catch (fun _ ->
     let file_names = Lwt_unix.files_of_directory directory in
@@ -45,6 +53,7 @@ let list_files (directory : string) : string list option Lwt.t =
     Lwt.return @@ Some file_names))
     (fun _ -> Lwt.return None)
 
+(** Start the CoHTTP server with the extracted handler. *)
 let start_server handler (port : int) : unit Lwt.t =
   let callback (connection : Cohttp_lwt_unix.Server.conn)
     (request : Cohttp.Request.t) (body : Cohttp_lwt_body.t)
