8d688164390a9d0671629175a3bf74a5f1cc4a0f
Guillaume Claret <dev@clarus.me>
2015-01-16 14:29:10 +0100

    Beginning of comments

--- Answer.v
+++ Answer.v
@@ -1,32 +1,48 @@
+(** Answers from the server. *)
 Require Import ListString.All.
 Require Http.
 Require Import Model.
 
-Module Raw.
-  Record t := New {
-    mime_type : LString.t;
-    cookies : Http.Cookies.t;
-    content : LString.t }.
-End Raw.
-
+(** Public pages (do not require to login). *)
 Module Public.
   Inductive t :=
   | Index (posts : list Post.Header.t)
   | PostShow (url : LString.t) (post : option Post.t).
 End Public.
 
+(** Private pages (require to login). *)
 Module Private.
   Inductive t :=
+  (** Form to add a post. *)
   | PostAdd
+  (** Confirm that a post is added. *)
   | PostDoAdd (is_success : bool)
+  (** Form to edit a post. *)
   | PostEdit (url : LString.t) (post : option Post.t)
+  (** Confirm that a post is edited. *)
   | PostDoEdit (url : LString.t) (is_success : bool)
+  (** Confirm that a post is deleted. *)
   | PostDoDelete (is_success : bool).
 End Private.
 
+(** Sum type of the answers. *)
 Inductive t :=
+(** Errors. *)
 | NotFound | WrongArguments | Forbidden
+(** Static content from the `static/` folder. *)
 | Static (mime_type : LString.t) (content : LString.t)
+(** Confirm that you are logged in or out. *)
 | Login | Logout
+(** Public pages. *)
 | Public (is_logged : bool) (page : Public.t)
+(** Private pages. *)
 | Private (page : Private.t).
+
+(** Raw answers after pretty-printing in `Render.v`. The return code is always
+    200 (OK). *)
+Module Raw.
+  Record t := New {
+    mime_type : LString.t;
+    cookies : Http.Cookies.t;
+    content : LString.t }.
+End Raw.
--- Computation.v
+++ Computation.v
@@ -4,14 +4,22 @@ Require Import Model.
 
 Local Open Scope type.
 
+(** System calls. *)
 Module Command.
   Inductive t :=
+  (** Read the content of a file. *)
   | ReadFile (file_name : LString.t)
+  (** Update (or create) a file with some content. *)
   | UpdateFile (file_name : LString.t) (content : LString.t)
+  (** Delete a file. *)
   | DeleteFile (file_name : LString.t)
+  (** List the posts in the `posts/` folder (as a list of `Post.Header.t`,
+      obtained after parsing). *)
   | ListPosts (directory : LString.t)
+  (** Write a message on the standard output. *)
   | Log (message : LString.t).
 
+  (** The type of an answer for a command depends on the value of the command. *)
   Definition answer (command : t) : Type :=
     match command with
     | ReadFile _ => option LString.t
@@ -22,38 +30,52 @@ Module Command.
     end.
 End Command.
 
+(** Computations with I/Os. *)
 Module C.
+  (** A computation can either return a pure value, or do a system call and wait
+      for the answer to run another computation. *)
   Inductive t (A : Type) : Type :=
   | Ret : forall (x : A), t A
   | Call : forall (command : Command.t), (Command.answer command -> t A) -> t A.
   Arguments Ret {A} _.
   Arguments Call {A} _ _.
 
+  (** Some optional notations. *)
   Module Notations.
+    (** We define an explicit apply function so that Coq does not try to expand
+        the notations everywhere. *)
     Definition apply {A B} (f : A -> B) (x : A) := f x.
 
-    Notation "'let!' x ':=' X 'in' Y" :=
-      (apply X (fun x => Y))
-      (at level 200, x ident, X at level 100, Y at level 200).
-
-    Notation "'let!' x : A ':=' X 'in' Y" :=
-      (apply X (fun (x : A) => Y))
-      (at level 200, x ident, X at level 100, A at level 200, Y at level 200).
-
-    Notation "'do_let!' X 'in' Y" :=
-      (apply X (fun _ => Y))
-      (at level 200, X at level 100, Y at level 200).
-
+    (** System call. *)
     Notation "'call!' answer ':=' command 'in' X" :=
       (Call command (fun answer => X))
       (at level 200, answer ident, command at level 100, X at level 200).
 
+    (** System call with typed answer. *)
     Notation "'call!' answer : A ':=' command 'in' X" :=
       (Call command (fun (answer : A) => X))
       (at level 200, answer ident, command at level 100, A at level 200, X at level 200).
 
+    (** System call ignoring the answer. *)
     Notation "'do_call!' command 'in' X" :=
       (Call command (fun _ => X))
       (at level 200, command at level 100, X at level 200).
+
+    (** This notation is useful to compose computations which wait for a
+        continuation. We do not have an explicit bind operator to simplify the
+        language and the proofs. *)
+    Notation "'let!' x ':=' X 'in' Y" :=
+      (apply X (fun x => Y))
+      (at level 200, x ident, X at level 100, Y at level 200).
+
+    (** Let with a typed answer. *)
+    Notation "'let!' x : A ':=' X 'in' Y" :=
+      (apply X (fun (x : A) => Y))
+      (at level 200, x ident, X at level 100, A at level 200, Y at level 200).
+
+    (** Let ignoring the answer. *)
+    Notation "'do_let!' X 'in' Y" :=
+      (apply X (fun _ => Y))
+      (at level 200, X at level 100, Y at level 200).
   End Notations.
 End C.
